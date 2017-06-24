abf2676ba3d1c100ad31ebeebdcb7d5688d9f89a
Bartosz Wesolowski <bartosz.wesolowski@hi-res.de>
2017-06-24 15:10:13 +0200

    Displaying line chart + parsing data from input fields.

--- visualization/js/main.js
+++ visualization/js/main.js
@@ -1,39 +1,100 @@
-(function($) {
-    
-  $('.calculate-button').click((event) => {
-      const formDiv = $(event.target).parent();
-      data = getDataToCalculate(formDiv);
-      getSimilarityResponse(data);
-  });
+var SimilarityModule = (function ($) {
 
-  getDataToCalculate = ($formDiv) => {
-    return {
-        setA: [0, 0, 1],
-        setB: [1, 0, 1],
-        method: 'minkowski',
-        config: {
-            r: 2
+    $('.calculate-button').click((event) => {
+        const $formDiv = $(event.target).parent();
+        const canvas = $formDiv.parent().find('.chart-canvas')[0];
+        dataToCalculate = getDataToCalculate($formDiv, ($configDiv) => {
+            let rValue = $configDiv.find('.config-r').val();
+            return {
+                r: parseInt(rValue) || 2
+            }
+        });
+        getSimilarityResponse(dataToCalculate, (response) => {
+            console.log('response: ' + JSON.stringify(response))
+            displayChart(canvas, response);
+        });
+        
+    });
+
+    /*
+        Extracts data from for div (set A, and set B)
+        configExtractor - function that will extract config object from config node
+    */
+    getDataToCalculate = ($formDiv, configExtractor) => {
+        let setAValue = $formDiv.find('.setA').val();
+        let setBValue = $formDiv.find('.setB').val();
+        return {
+            setA: getArrayFromString(setAValue),
+            setB: getArrayFromString(setBValue),
+            method: 'minkowski',
+            config: configExtractor($formDiv.find('.config'))
         }
     }
-  }
-  
-  getSimilarityResponse = (dataToCalculate) => {
-    console.log(JSON.stringify(dataToCalculate));
-    $.ajax({
-        type: "POST",
-        contentType: "application/json",
-        dataType   : "json",
-        crossDomain: true,
-        processData: false,
-        url: 'http://localhost:5000/fuzzy/similarity',
-        data: JSON.stringify(dataToCalculate),
-        success: function(data) {
-            alert(JSON.stringify(data));
-        },
-        error: function(data) {
-            console.log('error');
+
+    getSimilarityResponse = (dataToCalculate, success) => {
+        $.ajax({
+            type: "POST",
+            contentType: "application/json",
+            dataType: "json",
+            crossDomain: true,
+            processData: false,
+            url: 'http://localhost:5000/fuzzy/similarity',
+            data: JSON.stringify(dataToCalculate),
+            success: function (data) {
+                success(data);
+            },
+            error: function (data) {
+                console.log('error');
+            }
+        });
+    }
+
+    displayChart = ($canvas, similarityResponse) => {
+        var ctx = $canvas.getContext('2d');
+        var borderWidth = 5;
+        const labels = _.range(Math.max(similarityResponse.setA.length, similarityResponse.setB.length))
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [
+                    {
+                        label: 'Set A',
+                        data: similarityResponse.setA,
+                        fill: false,
+                        borderColor: "red",
+                        borderWidth: borderWidth
+                    }, 
+                    {
+                        label: 'Set B',
+                        data: similarityResponse.setB,
+                        fill: false,
+                        borderColor: "blue",
+                        borderWidth: borderWidth
+                    }
+                ]
+            }
+        });
+    }
+
+    getArrayFromString = (numbersString) => {
+        const result = [];
+        var splittedArray = numbersString.split(/\s+|,|;/);
+        console.log(JSON.stringify(splittedArray));
+        for (x of splittedArray) {
+            if (x) { //if not empty string
+                let parsed = parseFloat(x);
+                if (parsed === NaN) {
+                    throw 'Can not parse ' + x + " to float value";
+                }
+                result.push(parsed);
+            }
         }
-    });
-  }
-  
+        return result;
+    }
+
+    return {
+        getArrayFromString: getArrayFromString
+    }
+
 })($);
