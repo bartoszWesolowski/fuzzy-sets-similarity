d929dca4532ca65a2e2100d948e778d094edf111
Bartosz Wesolowski <bartosz.wesolowski@hi-res.de>
2017-09-25 20:05:11 +0200

    Fixed saving results to excel file

--- python/utils/resultprocessor.py
+++ python/utils/resultprocessor.py
@@ -11,13 +11,14 @@ class ExcelResultProcessor(ConnsoleResultProcessor):
         self.workbook = Workbook()
         self.sheet = self.workbook.active
         self.sheet.title = 'Fuzzy set similarity result'
-        self.sheet.append(range(numberOfSets))
         self.workbookFile = workbookFile
 
     def processResult(self, result, metadata):
         print "Result: {}, config: {}. Stored in cell: (row, col): ({}, {})".format(result, metadata.config,
                                                                                     metadata.setAindex,
                                                                                     metadata.setBindex)
+        #rows and columns are 1-index base, that means that first column has index equal to 1
+        #and setAindex and setBIndex are zero based,
         self.sheet.cell(column=metadata.setAindex + 1, row=metadata.setBindex + 1, value=result)
 
     def save(self):
