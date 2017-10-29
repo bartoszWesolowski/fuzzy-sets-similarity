f41ca087051c5092b411e0b30637073087dd6f46
Bartosz Wesolowski <bartosz.wesolowski@hi-res.de>
2017-10-29 18:10:58 +0100

    Similarity calculator using facades for getting right calculator object

--- python/utils/simCalculator.py
+++ python/utils/simCalculator.py
@@ -1,7 +1,10 @@
 from parsers.raw_configuration_parser import ConfigurationParser
 from utils import constants
+from simcalculators.similarity_calculator_factory import SimilarityCalculatorFactory
 
 configurationParser = ConfigurationParser()
+
+similarityCalculatorFactory = SimilarityCalculatorFactory()
 
 def calculateSimilarityFromRawConfig(A, B, rawConfig):
     """Calculates similarity of two sets A and B using method declared in config map.
@@ -9,8 +12,8 @@ def calculateSimilarityFromRawConfig(A, B, rawConfig):
     that will be parsed to desired format"""
     parsedConfig = configurationParser.validateAndParse(rawConfig)
     method = parsedConfig[constants.METHOD_PARAM_NAME]
-    calculator = constants.SIMILARITY_METHODS_MAP[method]
-    result = calculator(A, B, parsedConfig)
+    calculator = similarityCalculatorFactory.getSimilarityCalculator(method)
+    result = calculator.calculateSimilarity(A, B, parsedConfig)
     return result
 
 
@@ -19,6 +22,6 @@ def calculateSimilarityFromParsedConfig(A, B, parsedConfig):
     Config map must contain all parameters used to calculate similarity, has to be passed in format required
     by similarity calculator"""
     method = parsedConfig[constants.METHOD_PARAM_NAME]
-    calculator = constants.SIMILARITY_METHODS_MAP[method]
-    result = calculator(A, B, parsedConfig)
+    calculator = similarityCalculatorFactory.getSimilarityCalculator(method)
+    result = calculator.calculateSimilarity(A, B, parsedConfig)
     return result
