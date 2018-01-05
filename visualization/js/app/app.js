53f9ee17f3e337af3485f10efb04425982774ea5
Bartosz Wesolowski <bartosz.wesolowski@hi-res.de>
2018-01-05 12:16:17 +0100

    Array utils extracted to separate service.

--- visualization/js/app/app.js
+++ visualization/js/app/app.js
@@ -4,7 +4,7 @@
 
     var app = angular.module("fuzzy-sets", []);
 
-    app.directive("similarityCalculator", ['$http', function ($http) {
+    app.directive("similarityCalculator", ['$http', 'arrayUtils', function ($http, arrayUtils) {
         return {
             restrict: "E",
             templateUrl: function (elem, attr) {
@@ -13,11 +13,6 @@
             scope: {
                 methodConfig: '=',
                 similarityMethod: '='
-            },
-            link: function(scope, element, attrs) {
-                attrs.$observe('methodConfig', function(value) {
-                    console.log(value);
-                });
             },
             controller: function ($attrs) {
                 this.similarityOfSets = 0;
@@ -40,22 +35,15 @@
                     this.methodConfig.method = this.method
                     this.similarityData = angular.extend(this.defaultConfig, this.methodConfig)
 
-                    this.rawSetA = this.arrayToString(this.methodConfig.setA) || '0 0.5 1';
-                    this.rawSetB = this.arrayToString(this.methodConfig.setB) || '0 0.4 0.9';
+                    this.rawSetA = arrayUtils.arrayToString(this.methodConfig.setA) || '0 0.5 1';
+                    this.rawSetB = arrayUtils.arrayToString(this.methodConfig.setB) || '0 0.4 0.9';
 
                     this.calculateResult();
                 };
 
-                this.arrayToString = function (possiblyArray) {
-                    if (angular.isArray(possiblyArray)) {
-                        return possiblyArray.join(", ");
-                    }
-                    return '';
-                }
-
                 this.calculateResult = function () {
-                    this.similarityData.setA = this.parseRawSetInputToArray(this.rawSetA);
-                    this.similarityData.setB = this.parseRawSetInputToArray(this.rawSetB);
+                    this.similarityData.setA = arrayUtils.stringToArray(this.rawSetA);
+                    this.similarityData.setB = arrayUtils.stringToArray(this.rawSetB);
 
                     $http.post('http://localhost:5000/fuzzy/similarity', this.similarityData)
                         .then(
@@ -68,30 +56,6 @@
                                 alert('Error occurred! Error response: ' + JSON.stringify(errorResponse.data))
                             }
                         );
-                }
-
-                /**
-                 * Converts a string with floating point numbers
-                 * separated by white space, coma or colon to an
-                 * array of floats
-                 */
-                this.parseRawSetInputToArray = function (rawInput) {
-                    //TODO: test with var b = a.split(',').map(Number);
-                    //TODO: also convert this to service
-                    const result = [];
-                    var splittedArray = rawInput.split(/\s+|,|;/);
-                    console.log(JSON.stringify(splittedArray));
-                    for (x of splittedArray) {
-                        if (x) { //if not empty string
-                            let parsed = parseFloat(x);
-                            if (isNaN(parsed)) {
-                                console.log('Can not parse ' + x + " to float value. Ignoring this input.");
-                            } else {
-                                result.push(parsed);
-                            }
-                        }
-                    }
-                    return result;
                 }
 
             },
@@ -132,4 +96,42 @@
         ];
     });
 
+    app.factory('arrayUtils', function() {
+
+        /**
+         * Converts a string with floating point numbers
+         * separated by white space, coma or colon to an
+         * array of floats
+         */
+       const stringToArray = function (rawInput) {
+           //TODO: test with var b = a.split(',').map(Number);
+           //TODO: also convert this to service
+           const result = [];
+           var splittedArray = rawInput.split(/\s+|,|;/);
+           console.log(JSON.stringify(splittedArray));
+           for (x of splittedArray) {
+               if (x) { //if not empty string
+                   let parsed = parseFloat(x);
+                   if (isNaN(parsed)) {
+                       console.log('Can not parse ' + x + " to float value. Ignoring this input.");
+                   } else {
+                       result.push(parsed);
+                   }
+               }
+           }
+           return result;
+       }
+
+        const arrayToString = function (possiblyArray) {
+            if (angular.isArray(possiblyArray)) {
+                return possiblyArray.join(", ");
+            }
+            return '';
+        }
+
+        return {
+            stringToArray,
+            arrayToString
+        };
+    });
 })();
