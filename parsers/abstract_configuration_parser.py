55d31edeeb1009a5f984975701230dd0456655f9
Bartosz Wesolowski <bartosz.wesolowski887@gmail.com>
2018-10-30 16:55:31 +0100

    docs

--- parsers/abstract_configuration_parser.py
+++ parsers/abstract_configuration_parser.py
@@ -4,6 +4,15 @@ import abc as abstract
 class AbstractConfigurationParser(object):
     @abstract.abstractmethod
     def parse(self, rawConfigMap):
+        """
+        Parses raw String -> String map to String -> Object map and validates the map attributes - for example it 
+        could parse string parameter to int.
+        This method should throw a AttributeError in case config state is not valid.
+        Each implementation of this class is connected to one implementation of similarity calculator.
+        Map returned by this method should be a valid configuration that will be used in connected similarity config. 
+        :param rawConfigMap: string -> string map
+        :return: string -> object map
+        """
         return {}
 
     def parseToIntOrRaiseAttributeError(self, value, errorMessage="Could not parse value to int."):
--- simcalculators/abstract_similarity_calculator.py
+++ simcalculators/abstract_similarity_calculator.py
@@ -5,4 +5,11 @@ class AbstractSimilarityCalculator(object):
 
     @abstract.abstractmethod
     def calculateSimilarity(self, A, B, configuration):
+        """
+        Calculates similarity between two fuzzy sets.
+        :param A: first fuzzy set to compare
+        :param B: second fuzzy set to compare
+        :param configuration: map containing all parameters of similarity method
+        :return: a number from [0, 1] range that represents the similarity between two fuzzy sets
+        """
         pass
