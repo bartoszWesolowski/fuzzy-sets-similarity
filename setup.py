56776f588df4a478390128da077da93f1b6dbc1a
Bartosz Wesolowski <bartosz.wesolowski@cognifide.com>
2019-05-21 00:09:46 +0200

    Updated data set with example fuzzy set pairs. Added versions to requirements.

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
-#not tested yet
+# not tested yet
 setup(name='fssimilarity',
       version='0.1',
       description='Tool for calculating fuzzy set similarity',
       url='https://github.com/bartoszWesolowski/fuzzy-sets-similarity/tree/master/python',
-      author='Bartek W',
+      author='Bartek Wesolowski',
       author_email='fssimilarity@test.com',
       license='MIT',
       install_requires=[
-          'markdown', 'flask_cors', 'flask', 'numpy', 'openpyxl', 'sklearn'
+          'markdown==3.0.1', 'flask_cors==3.0.7', 'flask==1.0.2', 'numpy==1.16.2',
+          'openpyxl==2.6.1', 'scikit-learn==0.20.3'
       ],
       zip_safe=False)
